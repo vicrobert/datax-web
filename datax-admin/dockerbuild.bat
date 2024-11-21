875a8ba1b5e7589d9673c118cd1688c0fdc79484
yangjunbo <yangjunbo@ccccltd.cn>
2024-11-21 16:31:17 +0800

    springboot+docker适配版本

---
+++ datax-admin/dockerbuild.bat
@@ -0,0 +1,2 @@
+docker build -t datax-admin:latest .
+docker save -o datax-admin.tar datax-admin:latest
---
+++ datax-executor/dockerbuild.bat
@@ -0,0 +1,2 @@
+docker build -t datax-executor:latest .
+docker save -o datax-executor.tar datax-executor:latest
